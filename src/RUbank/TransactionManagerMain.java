2b2fd24036b7b767f2030be040bf57eed044d7b1
zef-yeh <145411258+zef-yeh@users.noreply.github.com>
2023-10-31 01:26:16 -0400

    added javafx and xml classes

---
+++ src/RUbank/TransactionManagerMain.java
@@ -0,0 +1,15 @@
+package RUbank;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TransactionManagerMain extends Application{
+
+    public static void main(String[]args){
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage){
+    }
+}
