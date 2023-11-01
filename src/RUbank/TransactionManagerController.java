fe0bbce030c55945784c750b8617443bf378bd9c
zef-yeh <145411258+zef-yeh@users.noreply.github.com>
2023-11-01 01:31:07 -0400

    added handlers

--- src/RUbank/TransactionManagerController.java
+++ src/RUbank/TransactionManagerController.java
@@ -2,16 +2,19 @@ package RUbank;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
-import javafx.scene.control.TextArea;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 public class TransactionManagerController {
 
     @FXML
     private GridPane accountTypeChooserGridPane;
+
+    @FXML
+    private RadioButton camdenButton;
 
     @FXML
     private RadioButton checkingButtonClick;
@@ -20,16 +23,37 @@ public class TransactionManagerController {
     private RadioButton collegeCheckingButtonClick;
 
     @FXML
+    private DatePicker dateOfBirth;
+
+    @FXML
+    private TextField firstName;
+
+    @FXML
+    private TextField lastName;
+
+    @FXML
     private GridPane locationTypeChooserGridPane;
 
     @FXML
-    private RadioButton loyalCustomerButton;
+    private CheckBox loyalCustomerButton;
 
     @FXML
     private RadioButton moneyMarketButtonClick;
 
     @FXML
+    private RadioButton newBrunswickButton;
+
+    @FXML
+    private RadioButton newarkButton;
+
+    @FXML
+    private Button openButton;
+
+    @FXML
     private RadioButton savingsButtonClick;
+
+    @FXML
+    private TextArea textArea;
 
     @FXML
     private ToggleGroup tgAccountType;
@@ -37,11 +61,6 @@ public class TransactionManagerController {
     @FXML
     private ToggleGroup tgLocation;
 
-    @FXML
-    private ToggleGroup tgLoyalCustomer;
-
-    @FXML
-    private TextArea textArea;
     @FXML
     void initialize(){
         locationTypeChooserGridPane.setDisable(true);
@@ -52,7 +71,6 @@ public class TransactionManagerController {
                 locationTypeChooserGridPane.setDisable(!"collegeCheckingButtonClick".equals(selectedButtonId));
             }
         });
-
         loyalCustomerButton.setDisable(true);
         tgAccountType.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue != null) {
@@ -60,12 +78,55 @@ public class TransactionManagerController {
                 loyalCustomerButton.setDisable(!"moneyMarketButtonClick".equals(selectedButtonId));
             }
         });
+        loyalCustomerButton.setDisable(true);
+        tgAccountType.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                String selectedButtonId = ((ToggleButton) newValue).getId();
+                loyalCustomerButton.setDisable(!"savingsButtonClick".equals(selectedButtonId));
+            }
+        });
 
     }
-
     @FXML
     void onCollegeCheckingButtonClick(ActionEvent event) {
 
+    }
+    @FXML
+    void openAccountButton(ActionEvent event) {
+        System.out.println(firstName.getText());
+        System.out.println(lastName.getText());
+
+        if(checkingButtonClick.isSelected()){
+            System.out.println("chjecking  selecetd!");
+        }
+        else if(collegeCheckingButtonClick.isSelected()){
+            if(newarkButton.isSelected()){
+                System.out.println("newy selected");
+            }
+            else if(newBrunswickButton.isSelected()){
+                System.out.println("newy brunswy selected");
+            }
+            else if(camdenButton.isSelected()){
+                System.out.println("cammy selected");
+            }
+            else {
+                System.out.println("select a campus!");
+            }
+        }
+        else if(savingsButtonClick.isSelected()){
+            System.out.println("savings are selected!");
+        }
+        else if(moneyMarketButtonClick.isSelected()){
+            System.out.println("moneymarket selected!");
+        }
+        else{
+            System.out.println("nothing is selecrted");
+        }
+    }
+    @FXML
+    void setCustomerStatusYes(ActionEvent event) {
+        loyalCustomerButton.setSelected(true);
+        loyalCustomerButton.setDisable(true);
     }
 
 
