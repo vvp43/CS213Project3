e4e3ff98f59a1913ef9e1af26e97981e633c0249
zef-yeh <145411258+zef-yeh@users.noreply.github.com>
2023-10-31 23:54:12 -0400

    added UI

--- src/RUbank/TransactionManagerMain.java
+++ src/RUbank/TransactionManagerMain.java
@@ -1,7 +1,13 @@
 package RUbank;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class TransactionManagerMain extends Application{
 
@@ -10,6 +16,11 @@ public class TransactionManagerMain extends Application{
     }
 
     @Override
-    public void start(Stage primaryStage){
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(TransactionManagerMain.class.getResource("TransactionManagerView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1368, 720);
+        stage.setTitle("Project 3 - Transaction Manager");
+        stage.setScene(scene);
+        stage.show();
     }
 }
